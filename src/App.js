3c417fb9efb3543cd9c3e19963d48c7b1fa26cdc
ansk1d <anasalkhaled@aol.com>
2022-08-21 14:46:11 +0300

    ezz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+const UserForm = (props) => {
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmpassword] = useState("");
+
+  const createUser = (e) => {
+    e.preventDefault();
+    const newUser = { firstName,lastName, email, password ,confirmPassword };
+    console.log("Welcome", newUser);
+    setFirstname("");
+    setLastname("");
+    setEmail("");
+    setPassword("");
+    confirmPassword("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <form onSubmit={createUser}>
+      <div>
+        <label>Frist Name: </label>
+        <input type="text" onChange={(e) => setFirstname(e.target.value)} value={ firstName }/>
+        <label>Last Name: </label>
+        <input type="text" onChange={(e) => setLastname(e.target.value)} value={ lastName }/>
 
-export default App;
+        <label>Email Address: </label>
+        <input type="text" onChange={(e) => setEmail(e.target.value)} value={ email }/>
+        <label>Password: </label>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} value={ password }/>
+        <label>Confirm Password: </label>
+        <input type="password" onChange={(e) => setConfirmpassword(e.target.value)} value={ confirmPassword }/>
+      </div>
+      <input type="submit" value="Create User" />
+    </form>
+    <div>
+      <p>First Name: {firstName}</p>
+      <p>Last Name: {lastName}</p>
+      <p>Email: {email}</p>
+      <p>Password: {password}</p>
+      <p>Confirm Password: {confirmPassword}</p>
+    </div>
+    </>
+  );
+};
+
+export default UserForm;
